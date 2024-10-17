2b247cc0710a114e4333e5ac3ca73f6ce282463f
Alex <alexmarkov827@gmail.com>
2024-10-17 22:31:20 +0500

    Код консольной программы индекс массы тела

--- src/Main.java
+++ src/Main.java
@@ -124,3 +124,26 @@ class InputDoubleVariablesDemo {
         JOptionPane.showMessageDialog(null, "Индекс массы тела: " + bmi);
     }
 }
+class InputVariablesConsoleDemo{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int now = 2024;
+        String name;
+        int age;
+        double height;
+        double mass;
+        System.out.print("Ваше имя: ");
+        name = input.nextLine();
+        System.out.print("Ваш возраст: ");
+        age = input.nextInt();
+        System.out.print("Ваш рост (в метрах): ");
+        height = input.nextDouble();
+        System.out.println("Масса тела (в киллограмах): ");
+        mass = input.nextDouble();
+        double bmi = mass/height/height;
+        System.out.println("Здравствуйте, "+name+"!");
+        System.out.println("Вы родились в "+(now-age)+" году!");
+        System.out.printf("Ваш индекс массы тела: %5.2f\n",bmi);
+    }
+}
