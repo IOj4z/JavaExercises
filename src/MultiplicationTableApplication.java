6f5a6939a615978ce42bd10cbb75da1ff5344e37
Alex <alexmarkov827@gmail.com>
2025-01-08 23:45:13 +0500

    MultiplicationTableApplication

---
+++ src/MultiplicationTableApplication.java
@@ -0,0 +1,26 @@
+class MultiplicationTableApplication {
+    public static void main(String[] args) {
+        int rows=9,cols=9;
+
+        int[][] table=new int[rows][cols];
+        String txt="\t|\t";
+        for(int i=1;i<=cols;i++){
+            txt+=i+"\t";
+        }
+
+        txt+="\n";
+
+        for(int i=1;i<=10+8*cols;i++){
+            txt+="-";
+        }
+        for(int i=0;i<table.length;i++){
+            txt+="\n"+(i+1)+"\t|\t";
+            for(int j=0;j<table[i].length;j++){
+                table[i][j]=(i+1)*(j+1);
+                txt += table[i][j]+"\t";
+            }
+        }
+        System.out.print("Таблица умножения:");
+        System.out.println(txt);
+    }
+}
